57e0a8ace33c94039d60c04c2243e368bc59162c
grmdeveloper <machado.gr20@gmail.com>
2019-07-17 10:55:08 -0300

    consulta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  unidades = [
+  	{	'nome':'São Jose',
+  		'especialidades':[
+  			{'nome':'Enfermeiro','d':10,'o':11},
+  			{'nome':'Clínico Geral','d':10,'o':11},
+  		],
+  	},
+  	{
+  		'nome':'Jacutinga',
+  		'especialidades':[
+  			{'nome':'Enfermeiro','d':11,'o':16},
+  			{'nome':'Clínico Geral','d':6,'o':14},
+  		],
+  	}
+  ];
 
+
+/*  especialidade:string = "Médico Cardiologista";
+  leitos_disponiveis:number = 12;
+  leitos_ocupados:number = 17;*/
 }
